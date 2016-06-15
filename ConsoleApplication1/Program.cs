baa5e5b408d5ebb6fe07d9ce0cf6449f0f3dbbef
oleks <oleks@DESKTOP-BKIUS7H>
2016-06-15 23:28:22 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задайте размер массива");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n];
+            int len = n;
+            Random r = new Random();
+            for (int i = 0; i < len; i++)
+            {
+                array[i] = r.Next(-100, 101);
+            }
+            Console.WriteLine("Массив до сортировки");
+            for (int i = 0; i < len; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+            QuickSorting.QuickSort(array, 0, len - 1);
+            Console.WriteLine("Массив после сортировки");
+            for (int i = 0; i < len; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+    }
+}
---
+++ ConsoleApplication1/QuickSorting.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class QuickSorting
+    {
+        public static void QuickSort(int[] array, int left, int right)
+        {
+            int min = left;
+            int max = right;
+            int mid = array[(left + right) / 2];
+            while (min <= max)
+            {
+                while ((min < right) && (array[min] < mid))
+                {
+                    min++;
+                }
+                while ((max > left) && (array[max]) > mid)
+                {
+                    max--;
+                }
+                if (min <= max)
+                {
+                    int temp = array[min];
+                    array[min] = array[max];
+                    array[max] = temp;
+                    min++;
+                    max--;
+                }
+                else
+                {
+
+                }
+            }
+            if (min < right)
+            {
+                QuickSort(array, min, right);
+            }
+            else
+            {
+
+            }
+            if (left < max)
+            {
+                QuickSort(array, left, max);
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
